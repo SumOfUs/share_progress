40c65aef9680306a6c0962f649d6cf814144c80e
Neal Donnelly <NealJMD@gmail.com>
2015-08-24 13:26:37 -0400

    write class methods with class << self idiom

--- lib/share_progress/button.rb
+++ lib/share_progress/button.rb
@@ -4,34 +4,36 @@ require 'share_progress/client'
 module ShareProgress
   class Button
 
-    def self.new
-    end
+    class << self
+      def new
+      end
 
-    def self.update
-    end
+      def update
+      end
 
-    def self.find(id)
-      Client.get endpoint('read'), { query: { id: id } }
-    end
+      def find(id)
+        Client.get endpoint('read'), { query: { id: id } }
+      end
 
-    def self.destroy(id)
-      Client.post endpoint('delete'), { query: { id: id } }
-    end
+      def destroy(id)
+        Client.post endpoint('delete'), { query: { id: id } }
+      end
 
-    def self.all(options={})
-      acceptable = [:offset, :limit]
-      Client.get endpoint, { query: filter_keys(options, acceptable) }
-    end
+      def all(options={})
+        acceptable = [:offset, :limit]
+        Client.get endpoint, { query: filter_keys(options, acceptable) }
+      end
 
-    private
+      private
 
-    def self.endpoint(method=nil)
-      extension = method.nil? ? "" : "/#{method}"
-      "/buttons#{extension}"
-    end
+      def endpoint(method=nil)
+        extension = method.nil? ? "" : "/#{method}"
+        "/buttons#{extension}"
+      end
 
-    def self.filter_keys(params, acceptable)
-      params.select{ |key, _| acceptable.include? key }
+      def filter_keys(params, acceptable)
+        params.select{ |key, _| acceptable.include? key }
+      end
     end
 
   end
