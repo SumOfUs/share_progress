172794df8e5b1e3adc0a0cd1ef1edbbd880525c2
Neal Donnelly <NealJMD@gmail.com>
2015-08-24 17:43:26 -0400

    start building out initialize and instance methods for Button

--- lib/share_progress/button.rb
+++ lib/share_progress/button.rb
@@ -4,6 +4,8 @@ require 'share_progress/client'
 module ShareProgress
   class Button
 
+    attr_accessor :id, :page_url, :page_title, :button_template, :share_button_html, :is_active
+
     class << self
       def create
       end
@@ -12,7 +14,9 @@ module ShareProgress
       end
 
       def find(id)
-        Client.get endpoint('read'), { query: { id: id } }
+        matches = Client.get endpoint('read'), { query: { id: id } }
+        raise ArgumentError.new("No button exists with id #{id}") if matches.size < 1
+        new_from_fields(matches[0])
       end
 
       def destroy(id)
@@ -21,7 +25,8 @@ module ShareProgress
 
       def all(options={})
         acceptable = [:offset, :limit]
-        Client.get endpoint, { query: filter_keys(options, acceptable) }
+        matches = Client.get endpoint, { query: filter_keys(options, acceptable) }
+        matches.map{ |match| new_from_fields(match) }
       end
 
       private
@@ -34,6 +39,24 @@ module ShareProgress
       def filter_keys(params, acceptable)
         params.select{ |key, _| acceptable.include? key }
       end
+
+      def new_from_fields(fields)
+        new(fields['id'],
+            fields['page_url'],
+            fields['page_title'],
+            fields['button_template'],
+            fields['share_button_html'],
+            fields['is_active'])
+      end
+    end
+
+    def initialize(id, page_url, page_title, button_template, share_button_html, is_active)
+      self.id = id
+      self.page_url = page_url
+      self.page_title = page_title
+      self.button_template = button_template
+      self.share_button_html = share_button_html
+      self.is_active = is_active
     end
 
   end
--- lib/share_progress/client.rb
+++ lib/share_progress/client.rb
@@ -6,5 +6,14 @@ module ShareProgress
 
     base_uri 'run.shareprogress.org/api/v1'
     default_params key: ENV['SHARE_PROGRESS_API_KEY']
+
+    class << self
+
+      def get(*args)
+        http_response = super(*args)
+        http_response['response'].nil? ? [] : http_response['response']
+      end
+
+    end
   end
 end
