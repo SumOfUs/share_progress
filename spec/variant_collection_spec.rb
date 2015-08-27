df3b20cc2449c75ffbc5f579bf335320ce4941ac
Neal Donnelly <NealJMD@gmail.com>
2015-08-27 13:29:07 -0400

    add many unimplemented specs to VariantCollection spec

--- spec/variant_collection_spec.rb
+++ spec/variant_collection_spec.rb
@@ -29,6 +29,8 @@ module ShareProgress
       it 'accepts one argument' do
         expect{ VariantCollection.new(basic_variants)}.not_to raise_error
       end
+
+      it 'calls update_variants'
     end
 
     describe 'update_variants' do
@@ -40,7 +42,8 @@ module ShareProgress
         end
       end
 
-      it 'leaves the same Variant instances in use after updating their properties' do
+      it 'leaves the same Variant instances in use after updating their properties with a list of Variants'
+      it 'leaves the same Variant instances in use after updating their properties with a hash' do
         expect(hollow_collection.variants.size).to eq 3
         v1, v2, v3 = hollow_collection.variants
         hollow_collection.update_variants(basic_variants)
@@ -58,16 +61,59 @@ module ShareProgress
         end
       end
 
-      it 'trims Variants to those included in update call' do
+      it 'trims Variants to those invluded in list of Variants'
+      it 'trims Variants to those included in hash' do
         original_variants = hollow_collection.variants
         hollow_collection.update_variants({'twitter' => [twitter_hash]})
         expect(hollow_collection.variants.size).to eq 1
         expect(original_variants).to include hollow_collection.variants[0]
       end
 
-      it 'can add some while removing others'
+      it 'can add some while Variants while removing others'
+      it 'can add many Variants all of one type'
+      it 'can add from a list of Variant objects'
+
+      it 'can update the button field by passing it referenced in a hash'
 
     end
 
+    describe 'add_or_update' do
+
+      it 'can add a TwitterVariant from a TwitterVariant object'
+      it 'can add a TwitterVariant from an attribute hash'
+      it 'can add a EmailVariant from a EmailVariant object'
+      it 'can add a EmailVariant from an attribute hash'
+      it 'can add a FacebookVariant from a FacebookVariant object'
+      it 'can add a FacebookVariant from an attribute hash'
+
+      # these should guard against duplication
+      it 'can update an existing TwitterVariant from a TwitterVariant object'
+      it 'can update an existing TwitterVariant from an attribute hash'
+      it 'can update an existing EmailVariant from a EmailVariant object'
+      it 'can update an existing EmailVariant from an attribute hash'
+      it 'can update an existing FacebookVariant from a FacebookVariant object'
+      it 'can update an existing FacebookVariant from an attribute hash'
+
+    end
+
+  describe 'serialize' do
+
+    it 'is formatted as a hash with each media type as a list of hashes'
+    it 'correctly serializes with just one type of media'
+    it 'correctly serizlizes with multiple instances of each type of media'
+    it 'reflects changes from update_variants'
+
+  end
+
+  describe 'remove' do
+
+    # in my implentation, you need id and type, but the ids on SP span
+    # type, so I reckon that we just should make it find by id
+    it 'removes a variant referenced by a hash with just id'
+    it 'removes two variants with the same id'
+
+    it 'removes a variant referenced by the variant object'
+    it 'does not remove anything when passed a different variant with the same id'
+  end
   end
 end
