8b25a48d916be182715be6a422610edf08eb1775
Neal Donnelly <NealJMD@gmail.com>
2015-09-16 19:33:35 -0600

    log the request made when reportin SP errors

--- lib/share_progress/client.rb
+++ lib/share_progress/client.rb
@@ -20,13 +20,20 @@ module ShareProgress
       private
 
       def format_response(http_response)
-        if http_response.code != 200 && http_response.code != 404
-          raise ApiError.new("Status #{http_response.code}: #{http_response['message']}")
-        end
+        check_api_error(http_response)
         formatted = http_response['response'].nil? ? [] : http_response['response']
         errors = http_response['success'] ? {} : http_response['message']
         formatted.each { |r| r['errors'] = errors }
         formatted
+      end
+
+      def check_api_error(http_response)
+        return if http_response.code < 300
+        return if http_response.code == 404 || http_response.code == 422
+        error_msg = "Status #{http_response.code}: #{http_response['message']}\n" +
+                    "Requesting: #{http_response.request.uri.to_s}\n" +
+                    "With Body: #{http_response.request.raw_body}"
+        raise ApiError.new(error_msg)
       end
 
     end
--- share_progress.gemspec
+++ share_progress.gemspec
@@ -29,5 +29,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'vcr'
 
   # Runtime Dependencies
-  spec.add_runtime_dependency 'httparty'
+  spec.add_runtime_dependency 'httparty', '>= 0.13'
 end
